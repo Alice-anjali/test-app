16f4ac639b91aaf92d155a8f527ca2e27fe05f38
Alice-anjali <alice.anjali1995@gmail.com>
2017-07-05 21:56:39 +0530

    plugin add

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 var index = require('./index');
 const app = express()
 
-app.use('/', index);
+app.get('/', function(req, res, next) {
+  console.log("works fine");
+});
+
+// app.use('/', index);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+//
+// router.get('/', function(req, res, next) {
+//
+//   module.exports = robot => {
+//     robot.on('pull_request.number', async context => {
+//       robot.log(context);
+//     });
+//   };
+//
+//   res.send("It works");
+// });
+//
+// module.exports = router;
 
-router.get('/', function(req, res, next) {
 
-  module.exports = robot => {
-    robot.on('pull_request.number', async context => {
-      // An issue was just opened.
-      robot.log(context);
-    });
-  };
-
-  res.send("It works");
-});
-
-module.exports = router;
+module.exports = robot => {
+  robot.on('pull_request.number', async context => {
+    robot.log(context);
+  });
+};
